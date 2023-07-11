e05ff36101641126510d84866df4cbb02514bac7
Patience-Fuglo <pfuglo@fordham.edu>
2023-07-10 20:56:27 -0400

    Created models and routes

--- api/index.js
+++ api/index.js
@@ -4,11 +4,15 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 const app = express();
 dotenv.config();
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 //mongoose.com connection to add try catch error
 const connect = async () => {
   try {
-    await mongoose.connect("process.env.MONGO");
+    await mongoose.connect(process.env.MONGO);
     //connected to mongoDB
     console.log("connected to mongoDB.");
   } catch (error) {
@@ -19,11 +23,28 @@ const connect = async () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected!");
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
 });
 
 //connect backend with localhost 8800
 app.listen(8800, () => {
+  connect();
   console.log("connected to backend.");
 });
